dd37e5324db676baf745c716bedf7471d4ab4958
FernandoFraile <82083270+FernandoFraile@users.noreply.github.com>
2023-03-30 17:29:39 +0200

    Menus en todas las paginas y arrelados problemas de cargar menu segun tamaño

--- scripts/general.js
+++ scripts/general.js
@@ -58,6 +58,127 @@ $(document).ready(function(){
 
 });
 
+//Funciones para el Menu
+
+/*
+function ponerFondoColor(){
+    if(window.location.href==="fitness.html"){
+        let elem = document.getElementsByClassName(".menuLista")[0];
+        console.log(elem.id);
+        //elem.style.backgroundColor = "#b2d8b2";
+    }
+}
+*/
+
+$(document).ready(function(){
+    cambiarColor();
+
+});
+
+function cambiarColor(){
+    const listaElementos = document.querySelectorAll('ul.menuLista > li');
+    listaElementos.forEach(elemento => {
+        if(elemento.id===window.document.title){
+            elemento.style.color = "#b2d8b2";
+            elemento.style.fontSize = "30px"
+        }
+    });
+}
+
+$(document).ready(function(){
+    cambiarMenuSegunTamanho(); 
+
+});
+
+//Funcion para comprobar el tamaño de la pagina y que meta un menu u otro
+
+
+
+
+
+function insertarMenu(id)
+{   id.style.right = "0px";
+    id.style.left = "0px";
+    id.style.backgroundColor = "black";
+    id.innerHTML=" <div class=\"menu\">\n" +
+        "        <ul class=\"menuLista\">\n" +
+        "            <li class=\"menuItem\" ><a class=\"refMenu\"  href=\"index.html\"><img src='../Imagenes/Logo-menu.png' alt='Gimnasio SportCompostela'></a> </li>\n" +
+        "            <li class=\"menuItem\" id=\"Fitness\"'><a class=\"refMenu\" href=\"fitness.html\">\n" +
+        "                FITNESS </a></li>\n" +
+        "            <li class=\"menuItem\" id=\"Nutricion\"><a class=\"refMenu\" href=\"nutricion.html\"> NURTRICIÓN </a></li>\n" +
+        "            <li class=\"menuItem\" id=\"Personal\"><a class=\"refMenu\" href=\"personal.html\"> PERSONAL</a></li>\n" +
+        "            <li class=\"menuItem\" id=\"Cuotas\"><a class=\"refMenu\" href=\"cuotas.html\"> CUOTAS</a></li>\n" +
+        "            <li class=\"menuItem\" id=\"Contacto\"><a class=\"refMenu\" href=\"contacto.html\"> CONTACTO</a></li>\n" +
+        "        </ul>\n" +
+        "    </div>";
+}
+
+
+window.addEventListener('scroll', function() {
+    let menuFijo = document.getElementById("menuFijo");
+    let menuImagen = document.getElementsByClassName("menu")[0];
+    let textoInicio = document.getElementsByClassName("textoInicio")[0];
+
+    if (window.scrollY >= 100) {
+        // Si el scroll es mayor que 100 creamos el menu fijo
+        insertarMenu(menuFijo);
+        cambiarColor();
+        menuFijo.style.position = "fixed";
+        menuFijo.style.zIndex = "100";
+        menuImagen.style.display = "none";
+
+        // Si el scroll es mayor que 100 cambiamos el color del menu fijo
+    }else if(window.scrollY < 100 && window.innerWidth > 1260) {
+        menuFijo.innerHTML = "";
+        menuImagen.style.display = "flex";
+    }else if(window.scrollY < 100 && window.innerWidth <= 1260 && window.innerWidth > 800) {
+        menuFijo.style.backgroundColor = "rgba(90, 90, 90, 0.5)";
+        textoInicio.style.top = "60%";
+        textoInicio.style.display = "inline-block";
+    }else if(window.scrollY < 100 && window.innerWidth < 800){
+        menuFijo.style.backgroundColor = "rgba(90, 90, 90, 0.5)";
+    }
+});
+
+// Añadimos un evento para que cuando la pantalla se haga pequeña se ejecute
+
+window.addEventListener("resize", function() {
+    cambiarMenuSegunTamanho();
+
+});
+
+function cambiarMenuSegunTamanho(){
+    let menuFijo = document.getElementById("menuFijo");
+    //let menuImagen = document.getElementsByClassName("menu")[0];
+    //let inicio = document.getElementsByClassName("inicio")[0];
+    let textoInicio = document.getElementsByClassName("textoInicio")[0];
+    let logoInicio = document.getElementsByClassName("logoInicio")[0];
+    let menuImagen = document.getElementsByClassName("menu")[0];
+
+    if (window.innerWidth <= 1260 && window.innerWidth > 800) {
+        insertarMenu(menuFijo);
+        menuFijo.style.backgroundColor = "rgba(90, 90, 90, 0.5)";
+        menuFijo.style.position = "fixed";
+        menuFijo.style.zIndex = "100";
+        logoInicio.style.display = "none";
+        menuImagen.style.display = "none";
+        textoInicio.style.top = "60%";
+
+    }else if(window.innerWidth < 800){
+        textoInicio.style.display = "none";
+
+    }else if(window.innerWidth > 1260) {
+        menuFijo.innerHTML="";
+        menuImagen.style.display = "flex";
+        textoInicio.style.display = "inline-block";
+        logoInicio.style.display = "inline-block";
+    }
+}
+
+//Fin de funciones para el menu
+
+
+
 
 
 
